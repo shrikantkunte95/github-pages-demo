dff97ccc30b083d2000dc27391e9450307f6a95a
shrikantkunte95 <shrikantkunte95@gmail.com>
2025-06-26 17:32:48 +0530

    Github pages deploy config

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  output: 'export',          // tells Next to generate static files
-  basePath: isProd ? '/github-pages-demo' : '',
-  assetPrefix: isProd ? '/github-pages-demo/' : '',
-  trailingSlash: true        // keeps routes working on GH Pages
-}
